f8ce4c92577b972a49ed1e28ca62973c93973f83
zhangli20080808 <791492434@qq.com>
2021-07-03 19:24:30 +0800

    版本号检查

--- core/core/bin/index.js
+++ core/core/bin/index.js
@@ -4,12 +4,11 @@
  * https://blog.csdn.net/qq_17175013/article/details/117449955
  */
 const importLocal = require('import-local')
-const utils = require('@zhangli-cli-dev/utils')
+// const utils = require('@zhangli-cli-dev/utils')
 
 if (importLocal(__filename)) {
   // /Users/zhangli/learning_code/zhangli-cli-dev/zhangli-cli-dev/core/core/bin/index.js
   require('npmlog').info('cli', '正在使用 zhangli-cli-dev 本地版本')
 } else {
-  console.log(__filename)
   require('../lib')(process.argv.slice(2))
 }
--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,8 +1,21 @@
 'use strict';
 
 module.exports = index;
+const pkg = require('../package.json')
+const log = require('@zhangli-cli-dev/log')
+/**
+ * require 支持加载的类型资源 .js/.json/.node
+ * .js -> module.exports/exports
+ * .json -> JSON.parse
+ * any -> 会默认使用js引擎去解析，当成一个js文件 比如 require('a.txt')
+ * .md是不行的，但是如果我们把内容改成一段js代码，就可以
+ */
+function index() {
+  checkPkgVersion()
+}
 
-function index(argv) {
-    // TODO
-  console.log('core',argv)
+function checkPkgVersion () {
+  log.notice('cli',pkg.version)
+  // log.success('test','success...')
+  // log.verbose('debug','debug...')
 }
