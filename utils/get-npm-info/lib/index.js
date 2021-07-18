ebce3998d1cf0bbb73c9d241047855e4c5da1779
zhangli20080808 <791492434@qq.com>
2021-07-18 22:42:30 +0800

    判断当前package是否存在 exist

--- utils/get-npm-info/lib/index.js
+++ utils/get-npm-info/lib/index.js
@@ -1,7 +1,7 @@
-'use strict';
-const urlJoin = require('url-join');
-const axios = require('axios');
-const semver = require('semver');
+'use strict'
+const urlJoin = require('url-join')
+const axios = require('axios')
+const semver = require('semver')
 
 /**
  *
@@ -10,56 +10,65 @@ const semver = require('semver');
  * https://registry.npm.taobao.org/@zhangli-cli-dev/core
  * https://registry.npmjs.org/@zhangli-cli-dev/core
  */
-function getNpmInfo(npmName, registry) {
+function getNpmInfo (npmName, registry) {
   if (!npmName) {
-    return null;
+    return null
   }
-  const registryUrl = registry || getDefaultRegistry();
-  const npmInfoUrl = urlJoin(registryUrl, npmName);
+  const registryUrl = registry || getDefaultRegistry()
+  const npmInfoUrl = urlJoin(registryUrl, npmName)
   return axios
     .get(npmInfoUrl)
     .then((res) => {
       if (res.status === 200) {
-        return res.data;
+        return res.data
       }
     })
     .catch((err) => {
-      return Promise.reject(err);
-    });
+      return Promise.reject(err)
+    })
 }
 
-function getDefaultRegistry(isOriginal = false) {
+function getDefaultRegistry (isOriginal = false) {
   return isOriginal
     ? 'https://registry.npmjs.org/'
-    : 'https://registry.npm.taobao.org';
+    : 'https://registry.npm.taobao.org'
 }
 
-async function getNpmVersions(npmName, registry) {
-  const data = await getNpmInfo(npmName, registry);
+async function getNpmVersions (npmName, registry) {
+  const data = await getNpmInfo(npmName, registry)
   if (data) {
-    return Object.keys(data.versions);
+    return Object.keys(data.versions)
   } else {
-    return [];
+    return []
   }
 }
 
-function getSemverVersions(baseVersion, versions) {
+function getSemverVersions (baseVersion, versions) {
   return versions
     .filter((version) => semver.satisfies(version, `^${baseVersion}`))
-    .sort((a, b) => semver.gt(b, a));
+    .sort((a, b) => semver.gt(b, a))
   // 排序逻辑 npm返回的api可能没有排序
   // [ '1.0.5', '1.0.4' ]
 }
 
-async function getNpmSemverVersion(baseVersion, npmName, registry) {
-  const versions = await getNpmVersions(npmName, registry);
-  const newVersion = getSemverVersions(baseVersion, versions);
+async function getNpmSemverVersion (baseVersion, npmName, registry) {
+  const versions = await getNpmVersions(npmName, registry)
+  const newVersion = getSemverVersions(baseVersion, versions)
   if (newVersion && newVersion.length > 0) {
-    return newVersion[0];
+    return newVersion[0]
   }
+}
+
+async function getNpmLatestVersion (npmName, registry) {
+  let versions = await getNpmVersions(npmName, registry)
+  if (versions) {
+    return versions.sort((a, b) => semver.gt(b, a))[0]
+  }
+  return null
 }
 
 module.exports = {
   getNpmSemverVersion,
-  getDefaultRegistry
-};
+  getDefaultRegistry,
+  getNpmLatestVersion
+}
