41231e2c767a9fe76b5bc7d10e7162098b7c5349
zhangli20080808 <791492434@qq.com>
2022-10-12 22:50:13 +0800

    核心流程 - 命令注册init，exec文件创建

--- cli-test/zl-cli-test/bin/commander.js
+++ cli-test/zl-cli-test/bin/commander.js
@@ -43,41 +43,48 @@ service
 
 program.addCommand(service);
 
-// zl-test install init -> zhangli-cli init
-// program
-//   .command('install [name]', 'install package', {
-//     executableFile: 'zhangli-cli',
-//     // isDefault: true,
-//     hidden: true,
-//   })
-//   .alias('i');
+// zl-cli-test install(相当于executableFile对应的脚手架) init -> zl-cli-test  init ()多个脚手架串行的使用
+program
+  .command('install [name]', 'install package', {
+    // 表示可以直接修改可执行文件 
+    // Error: 'zl-cli-test-install' does not exist
+    // Error: 'zhangli-cli-install' does not exist
 
-// 
-// program
-//   .arguments('<cmd> [options]')
-//   .description('test command', {
-//     cmd: 'command to run',
-//     options: 'options for command',
-//   })
-//   .action(function(cmd, options) {
-//     console.log(cmd, options);
-//   });
+    // executableFile: 'zhangli-cli-install',
+    executableFile: 'zhangli-cli-dev',
+    // isDefault: true, //  zhangli-cli-dev默认的执行命令，false的话会继续下面的逻辑
+    hidden: true, // 将当前的这条命令隐藏起来，在控制台
+  })
+  .alias('i');
+
+// 对命令的注册进行自动匹配-除了上面已经注册过的命令，其他的命令都会命中到arguments中
+program
+  .arguments('<cmd> [options]')
+  .description('test command', {
+    cmd: 'command to run',
+    options: 'options for command',
+  })
+  .action(function(cmd, options) {
+    console.log(cmd, options); 
+    // zl-cli-test test 111-> test 111
+    // zl-cli-test-> error: missing required argument 'cmd' 强制输入,要求用户必须传入一个参数
+  });
 
 // 高级定制1：自定义help信息
 // program.helpInformation = function() {
 //   return '';
 // };
-// program.on('--help', function() {
-//   console.log('your help information');
-// });
+program.on('--help', function() {
+  console.log('your help information');
+});
 
-// 高级定制2：实现debug模式
-// program.on('option:debug', function () {
-//   if (program.debug) {
-//     process.env.LOG_LEVEL = 'verbose';
-//   }
-//   console.log(process.env.LOG_LEVEL);
-// });
+// 高级定制2：实现debug模式 -》 早于命令之前
+program.on('option:debug', function () {
+  if (program.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  }
+  console.log(process.env.LOG_LEVEL);
+});
 
 // // 高级定制3：对未知命令监听
 // program.on('command:*', function (obj) {
